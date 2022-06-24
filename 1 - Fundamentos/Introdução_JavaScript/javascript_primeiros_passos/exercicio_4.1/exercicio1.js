84538c4444817f805ae3bafddfd6c27c09d43a2e
HenriqueLeardi <hojegamepvp@gmail.com>
2022-06-23 22:58:07 -0300

    5-programas-para-operação-aritmética-básica

---
+++ 1 - Fundamentos/Introdução_JavaScript/javascript_primeiros_passos/exercicio_4.1/exercicio1.js
@@ -0,0 +1,17 @@
+const a = 10;
+const b = 14;
+
+let soma = 10 + 14;
+console.log(soma);
+
+let subtrair = 10 - 14;
+console.log(subtrair);
+
+let multiplicar = 10 * 14;
+console.log(multiplicar);
+
+let divisao = 10 / 14;
+console.log(divisao);
+
+let modulo = 10 % 14;
+console.log(modulo);
